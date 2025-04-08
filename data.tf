4d9dd41b318168af8fe1380d446cb8de3a30ee21
Damian Tykalowski <damian.tykalowski@tsh.io>
2025-04-08 08:24:08 +0200

    fix permissions

--- data.tf
+++ data.tf
@@ -26,6 +26,12 @@ data "aws_iam_policy_document" "app_runner_access_role_assume_policy" {
   }
 }
 
+locals {
+  all_secret_arns     = values(var.runtime_environment_secrets)
+  secretsmanager_arns = [for arn in local.all_secret_arns : contains(arn, ":secretsmanager:") ? arn : ""]
+  ssm_parameter_arns  = [for arn in local.all_secret_arns : contains(arn, ":ssm:") ? arn : ""]
+}
+
 data "aws_iam_policy_document" "app_runner_instance_policy" {
   dynamic "statement" {
     for_each = length(var.application_buckets) > 0 ? [1] : []
@@ -52,37 +58,30 @@ data "aws_iam_policy_document" "app_runner_instance_policy" {
     }
   }
 
-  statement {
-    sid    = "SecretManagerPolicy"
-    effect = "Allow"
-    actions = [
-      "secretsmanager:GetSecretValue",
-    ]
-
-    resources = ["arn:aws:secretsmanager:*:*:secret:*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/app_runner_service"
-      values   = [var.service_name]
-    }
-  }
-  statement {
-    sid    = "SSMPolicy"
-    effect = "Allow"
-    actions = [
-      "ssm:GetParameter",
-      "ssm:GetParameters",
-    ]
-    resources = ["arn:aws:ssm:*:*:parameter/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:ResourceTag/app_runner_service"
-      values   = [var.service_name]
+  dynamic "statement" {
+    for_each = length(local.secretsmanager_arns) > 0 ? [1] : []
+    content {
+      sid    = "SecretManagerPolicy"
+      effect = "Allow"
+      actions = [
+        "secretsmanager:GetSecretValue",
+      ]
+      resources = local.secretsmanager_arns
     }
   }
 
+  dynamic "statement" {
+    for_each = length(local.ssm_parameter_arns) > 0 ? [1] : []
+    content {
+      sid    = "SSMPolicy"
+      effect = "Allow"
+      actions = [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+      ]
+      resources = local.ssm_parameter_arns
+    }
+  }
 }
 
 data "aws_iam_policy_document" "app_runner_access_policy" {
