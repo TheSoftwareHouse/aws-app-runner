036ac0c99c60a87be58905a7cee51e248010d3c3
Damian Tykalowski <damian.tykalowski@tsh.io>
2025-04-08 13:30:22 +0200

    add fqdn to outputs

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "service_url" {
   description = "The publicly accessible URL of the App Runner service."
 }
 
+output "service_fqdn" {
+  value       = aws_apprunner_service.this.service_url
+  description = "The FQDN of the App Runner service."
+}
+
 output "service_status" {
   value       = aws_apprunner_service.this.status
   description = "The current operational status of the App Runner service (e.g., RUNNING, FAILED, etc.)."
